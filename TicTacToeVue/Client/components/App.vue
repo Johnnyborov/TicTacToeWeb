134dfa171b48db3ff6ef90dd5204fe66ee41abbb
Johnny Borov <JohnnyBorov@gmail.com>
2019-01-04 10:39:56 +0300

    created basic vue project template

---
+++ TicTacToeVue/Client/components/App.vue
@@ -0,0 +1,22 @@
+﻿<template>
+<div id="app">
+  <h2>{{message}}</h2>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Basic Store Project Template'
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: Verdana;
+    color: navy;
+  }
+</style>
---
+++ TicTacToeVue/webpack.config.js
@@ -0,0 +1,38 @@
+﻿/// <binding BeforeBuild='Run - Production' />
+var path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin'); // плагин для загрузки кода Vue
+
+module.exports = {
+  entry: './Client/main.js',
+  output: {
+    path: path.resolve(__dirname, './wwwroot/js'),
+    publicPath: '/wwwroot/js/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }, {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'wwwroot'),
+    publicPath: '/js/',
+    historyApiFallback: {
+      index: 'index.html'
+    },
+    watchContentBase: true,
+    compress: true
+  }
+}
