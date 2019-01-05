36b33258c877a90cd5ef51fc6b02e50571c45d1f
Johnny Borov <JohnnyBorov@gmail.com>
2019-01-06 00:13:14 +0300

    added cells resizing

--- TicTacToeVue/Client/components/GameField.vue
+++ TicTacToeVue/Client/components/GameField.vue
@@ -1,19 +1,15 @@
 <template>
 <div id="game-field">
-  <canvas id="canvas" ref="canvas">
-    <cells-renderer></cells-renderer>
-  </canvas>
+  <canvas id="canvas" ref="canvas"></canvas>
 </div>
 </template>
 
 <script>
 import Resources from '../resources/resources.js'
-import CellsRenderer from './CellsRenderer.vue'
 
 export default {
   data() {
     return {
-      message: 'Basic Store Project Template',
       context: null,
       cross: null,
       nought: null,
@@ -21,18 +17,14 @@ export default {
     }
   },
 
-  components: {
-    'cells-renderer': CellsRenderer
-  },
-
   created() {
     window.addEventListener("resize", this.resizeHandler)
-    window.addEventListener('load', this.ready)
+    window.addEventListener('load', this.loadedHandler)
   },
   
   destroyed() {
     window.removeEventListener("resize", this.resizeHandler)
-    window.removeEventListener('load', this.ready)
+    window.removeEventListener('load', this.loadedHandler)
   },
 
   mounted() {
@@ -59,15 +51,45 @@ export default {
       this.$refs['canvas'].width = this.$refs['canvas'].parentElement.clientWidth
       this.$refs['canvas'].height = this.$refs['canvas'].parentElement.clientHeight
 
-      this.$children.forEach(child => {
-        child.$forceUpdate();
-      });
+      this.drawCells()
     },
 
-    ready: function() {
-      this.$children.forEach(child => {
-        child.$forceUpdate();
-      });
+    loadedHandler: function() {
+      this.drawCells()
+    },
+
+    drawCells: function() {
+      let ctx = this.context 
+
+      let halfCellBorderWidth = Math.round(ctx.canvas.width / 40 / 2)
+      let cellBorderWidth = 2 * halfCellBorderWidth
+      let imgSize = Math.round((ctx.canvas.width - cellBorderWidth*(3 * 2 + (3-1))) / 3)
+      let imgStep = (imgSize + 3 * cellBorderWidth)
+
+      ctx.lineWidth = cellBorderWidth
+
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          ctx.strokeRect(halfCellBorderWidth+imgStep*j, halfCellBorderWidth+imgStep*i, imgSize+2*halfCellBorderWidth, imgSize+2*halfCellBorderWidth)         
+
+          let cell;
+          switch(this.$store.state.gameEngine.cells[i*3+j]) {
+            case 0:
+              cell = this.clear
+              break
+            case 1:
+              cell = this.cross
+              break
+            case 2:
+              cell = this.nought
+              break
+            default:
+              throw "Invalid cell value"
+          }
+
+          ctx.drawImage(cell, cellBorderWidth+imgStep*j, cellBorderWidth+imgStep*i, imgSize, imgSize) 
+        }    
+      }
     }
   }
 }
